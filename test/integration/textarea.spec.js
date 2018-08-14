93191f26bf4419c084f54a2b628109a8e0410218
Hosein Khansari <hrk.x11@gmail.com>
2018-08-14 16:10:18 +0430

    cleanup tests

--- test/integration/textarea.spec.js
+++ test/integration/textarea.spec.js
@@ -1,10 +1,10 @@
 /// <reference types='Cypress' />
 
 it('works with <textarea>', () => {
-cy.visit('/textarea')
-	.get('textarea')
-	.type('a line{enter}another line')
-	.get('div[r-text]')
-	.should('not.contain', '\n')
-	.get('div[r-text]>br').should('exist')
+	cy.visit('/textarea')
+		.get('textarea')
+		.type('a{enter}b')
+		.get('div[r-text]')
+		.should('not.contain', '\n')
+		.should('have.html', 'a<br>b')
 })
