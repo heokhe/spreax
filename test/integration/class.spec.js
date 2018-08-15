699a571c952d86e831af8920232048ef55953c44
Hosein Khansari <hrk.x11@gmail.com>
2018-08-15 16:04:57 +0430

    improve class directive test and exmaple

--- test/integration/class.spec.js
+++ test/integration/class.spec.js
@@ -3,9 +3,10 @@
 describe('class directive', () => {
 	it('works', () => {
 		cy.visit('/class')
-			.get('h1').should('not.have.class', 'active ud')
-			.get('button').first().click()
+			.get('h1').should('not.have.class', 'ud')
 			.get('h1').should('have.class', 'active')
+			.get('button').first().click()
+			.get('h1').should('not.have.class', 'active')
 			.get('button').last().trigger('mouseover')
 			.get('h1').should('have.class', 'ud')
 	})
