4c245fb78af39bf6801b44ccc06f883f0cdff8c3
Hosein Khansari <hrk.x11@gmail.com>
2018-08-16 13:09:53 +0430

    improve watchers test

--- test/integration/watchers.spec.js
+++ test/integration/watchers.spec.js
@@ -6,5 +6,7 @@ describe('watchers', () => {
 			.get('#t').should('have.text', '0')
 			.get('button').click()
 			.get('#t').should('have.text', '1')
+			.get('button').click()
+			.get('#t').should('have.text', '2')
 	})
 })
