32a52d221fcba7fdc6d13b8fe971e519d610aef1
Hosein Khansari <hrk.x11@gmail.com>
2018-11-30 23:21:16 +0330

    improve watchers' tests

--- test/integration/watcher.spec.js
+++ test/integration/watcher.spec.js
@@ -1,14 +1,17 @@
 describe('Watchers', () => {
 	it('works', () => {
 		cy.window().then(({console: c, instance}) => {
-			cy.spy()
 			cy.stub(c, 'log')
-			cy.wrap(c).its('log').should('not.be.called').then(() => {
-				instance.name = 'Jack'
-			}).should('be.calledWithExactly', 'Jack', 'Hosein').then(() => {
-				const { name } = instance
-				instance.name = name
-			}).should('be.calledOnce')
+
+			cy.wrap(c).its('log')
+				.should('not.be.called').then(() => {
+					const oldName = instance.name, newName = 'Jack'
+					instance.name = newName
+					expect(c.log).to.be.calledWithExactly(newName, oldName)
+				}).then(() => {
+					// eslint-disable-next-line no-self-assign
+					instance.name = instance.name
+				}).should('be.calledOnce')
 		})
 	})
 })
