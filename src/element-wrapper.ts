ae413b3bc85a30bcd6781284c06f33a63bba6338
Hosein Khansari <hrk.x11@gmail.com>
2020-01-08 21:46:15 +0330

    Store nodes in ElementWrapper.nodes

--- src/element-wrapper.ts
+++ src/element-wrapper.ts
@@ -4,15 +4,31 @@ import { TextNodeWrapper } from "./text-node-wrapper";
 
 export class ElementWrapper<T, E extends Element = Element> extends Subscriber<T> {
   el: E;
+  nodes: TextNodeWrapper<T>[];
   constructor(element: E) {
     super();
     this.el = element;
+    this.nodes = this.getNodes();
   }
 
-  nodes() {
+  getNodes() {
     return [...this.el.childNodes]
       .filter(node => node.nodeType === TEXT_NODE)
       .map(node => new TextNodeWrapper<T>(node))
       .filter(node => node.dependencies.length > 0)
+  }
+
+  directives() {
+    const { el } = this,
+      each = el.getAttribute('@each'),
+      [variableName, arrayName] = each?.split(' in ') ?? [];
+    return {
+      bind: el.getAttribute('@bind') ?? undefined,
+      each: each ? { variableName, arrayName } : undefined
+    }
+  }
+
+  destroy() {
+    this.el.remove();
   }
 }
