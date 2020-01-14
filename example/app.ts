ea9e274fd728ff726d6dced708186e2540c25660
Hosein Khansari <hrk.x11@gmail.com>
2020-01-14 18:05:22 +0330

    [example] add window size

--- example/app.ts
+++ example/app.ts
@@ -1,20 +1,13 @@
-import Spreax, { state, computed, setIndex, push } from "../src";
+import Spreax, { state, computed } from "../src/index";
+
+const windowSize = computed(() => window.innerWidth);
+window.addEventListener('resize', () => windowSize.compute());
+const isTablet = computed(() => windowSize.value >= 768);
 
 const el = document.getElementById('app');
 const n = state(2);
-const dbl = computed(() => n.value * 2);
-const hlf = computed(() => (dbl.value / 4).toFixed(1));
-const pow2 = computed(() => n.value ** 2);
-const sqrt = computed(() => Math.sqrt(n.value).toFixed(2));
-const array = state([1, 2, 3]);
-const dblarray = computed(() => array.value.map(x => x * n.value))
-const app = new Spreax(el, { n, dbl, hlf, pow2, sqrt, array, dblarray });
-
-setIndex(array, 0, 7);
-push(array, 9);
-push(array, 10);
-push(array, 11);
-push(array, -1);
-setIndex(array, 2, 85);
+const array = state([1, 2, 3, 9, 7]);
+const dblarray = computed(() => array.value.map(x => x * n.value));
+const app = new Spreax(el, { n, array, dblarray, windowSize, isTablet });
 
 globalThis.app = app;
