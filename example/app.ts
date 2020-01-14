738df742b5c28f5eda03c7060dbcd0fdda36ac27
Hosein Khansari <hrk.x11@gmail.com>
2020-01-14 18:45:05 +0330

    [example] Add example for actions

--- example/app.ts
+++ example/app.ts
@@ -1,4 +1,4 @@
-import Spreax, { state, computed } from "../src/index";
+import Spreax, { state, computed, inc } from "../src/index";
 
 const windowSize = computed(() => window.innerWidth);
 window.addEventListener('resize', () => windowSize.compute());
@@ -8,6 +8,15 @@ const el = document.getElementById('app');
 const n = state(2);
 const array = state([1, 2, 3, 9, 7]);
 const dblarray = computed(() => array.value.map(x => x * n.value));
-const app = new Spreax(el, { n, array, dblarray, windowSize, isTablet });
+const app = new Spreax(
+  el,
+  { n, array, dblarray, windowSize, isTablet },
+  {
+    increase(event) {
+      console.log(event.target)
+      inc(n);
+    }
+  }
+);
 
 globalThis.app = app;
