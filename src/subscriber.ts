1f431b26487c22045fb6e39d11e030756c840834
Hosein Khansari <hrk.x11@gmail.com>
2020-01-15 13:05:02 +0330

    [docs] write a bit of docs for subscribeTo

--- src/subscriber.ts
+++ src/subscriber.ts
@@ -8,8 +8,13 @@ export abstract class Subscriber<C> {
     this.context = {} as Variables<C>;
   }
 
+  /**
+   * Subscribes the object to a `Subscribable`.
+   * Silently fails if `name` already exists.
+   */
   subscribeTo<K extends keyof C, V extends Variable<C[K]>>(name: K, variable: V) {
-    if (!(name in this.context)) this.context[name] = variable;
+    if (!(name in this.context))
+      this.context[name] = variable;
   }
 
   listenFor<K extends keyof C>(name: K, callback: Subscription<C[K]>, immediate = false) {
