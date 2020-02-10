df0e02de2510110f7248b2740e11dcb6320aa73f
Hosein Khansari <hrk.x11@gmail.com>
2020-02-10 18:17:29 +0330

    Implement @class directive.

---
+++ src/directives/handlers/class.ts
@@ -0,0 +1,18 @@
+import { DirectiveHandler, DirectiveMatch } from '../handler';
+
+export class ClassHandler<T> extends DirectiveHandler<T> {
+  name = 'class';
+
+  parameters = true;
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  init() {}
+
+  handle(value: any, { parameter: className }: DirectiveMatch) {
+    const { classList } = this.el;
+    if (value)
+      classList.add(className);
+    else
+      classList.remove(className);
+  }
+}
