5630749d2a1f5bf8fb5ece6d34eef424ea549105
Hosein Khansari <hrk.x11@gmail.com>
2020-02-25 20:46:17 +0330

    Add helper functions for "secret attributes"
    @attr converts every value to a string.
    Spreax needs to know the real values,
    So it adds a `_$secretAttriubutes_` to each element, and stores the values there.
    those values are used in component props.

--- src/dom.ts
+++ src/dom.ts
@@ -1,10 +1,36 @@
-export function createElementTree(root: HTMLElement, components = false) {
-  const elements = (!components || root.tagName.includes('-')) ? [root] : [];
+export const isComponent = (element: Element) =>
+  element.tagName.includes('-');
+
+export function createElementTree(root: HTMLElement, componentsOnly = false) {
+  const elements = (!componentsOnly || isComponent(root)) ? [root] : [];
   for (const child of root.children)
-    elements.push(...createElementTree(child as HTMLElement, components));
+    elements.push(...createElementTree(child as HTMLElement, componentsOnly));
   return elements;
 }
 
 export function elementExistsInDOM(el: HTMLElement) {
   return el.ownerDocument.contains(el);
+}
+
+declare global {
+  interface Element {
+    _$secretAttrs_: {
+      [x: string]: any;
+    };
+  }
+}
+
+
+export function getSecretAttribute(element: HTMLElement, attrName: string) {
+  return element._$secretAttrs_?.[attrName];
+}
+
+export function setSecretAttribute(element: HTMLElement, attrName: string, value: any) {
+  if (!element._$secretAttrs_)
+    element._$secretAttrs_ = {};
+  element._$secretAttrs_[attrName] = value;
+}
+
+export function hasSecretAttribute(element: HTMLElement, attrName: string) {
+  return attrName in (element._$secretAttrs_ ?? {});
 }
