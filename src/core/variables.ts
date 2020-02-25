ca19238331876490bda86fa3074f0885858a5dab
Hosein Khansari <hrk.x11@gmail.com>
2020-02-25 22:33:37 +0330

    Fix mistake in getVariablesFromObject()

--- src/core/variables.ts
+++ src/core/variables.ts
@@ -7,7 +7,7 @@ export type Variables<T> = {
   [x in keyof T]: Variable<T[x]>;
 };
 
-export function getVariablesFromObject<T>(variables: T): Variable<T[keyof T]>[] {
+export function getVariablesFromObject<T>(variables: Variables<T>): Variable<T[keyof T]>[] {
   return Object.values(variables);
 }
 
